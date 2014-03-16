0f3b4a2d6904c9ebcdeb734e26db558bd7de8cdc
Tony <zippo2021@gmail.com>
2014-03-16 18:41:11 +0400

    Gui started

--- summerreg/summerreg/settings.py
+++ summerreg/summerreg/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gui',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+    )
