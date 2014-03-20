f825f74dc882a4871c7b73af4dfb4162968e4be5
Tony <zippo2021@gmail.com>
2014-03-20 14:33:53 +0400

    Registartion and Authorisation now works

--- summerreg/gui/views.py
+++ summerreg/gui/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 
 
 def index(request):
+  if request.user.is_authenticated():
+    
+    t = get_template('gui_index.html')
+    html = t.render(Context())
+    return HttpResponse(html)
   
-  t = get_template('gui_index.html')
-  html = t.render(Context())
-  return HttpResponse(html)
-
+  else: 
+    return redirect('/accounts/login/')
 def data_load(request):
 
   
