aecdefd7989a150088c263e3e5da5736b920d26b
Tony <zippo2021@gmail.com>
2014-03-20 19:19:30 +0400

    Registration and authorisation ready

--- summerreg/gui/views.py
+++ summerreg/gui/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
@@ -6,10 +6,12 @@ from django.template import Context
 
 def index(request):
   
-  t = get_template('gui_index.html')
-  html = t.render(Context())
-  return HttpResponse(html)
-
+  if request.user.is_authenticated():
+    t = get_template('gui_index.html')
+    html = t.render(Context())
+    return HttpResponse(html)
+  else:
+    return redirect('/accounts/login')
 def data_load(request):
 
   
