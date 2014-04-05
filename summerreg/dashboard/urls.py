d242728d00641b5a26410968da9f9162127db034
Tony <zippo2021@gmail.com>
2014-04-05 17:18:32 +0400

    Added documents to registration

--- summerreg/dashboard/urls.py
+++ summerreg/dashboard/urls.py
@@ -5,5 +5,7 @@ from dashboard import views
 urlpatterns = patterns('',
     url(r'^$', views.dash_index, name='dash_index'),
     url(r'^summer_registration$', views.summer_registration, name='summer_registration'),
-    url(r'^user_data', views.user_data_viewer, name='user_data'),        
+    url(r'^user_data', views.user_data_viewer, name='user_data'), 
+    url(r'^doc_type_select', views.doc_type_select, name='doc_type_select'),
+    url(r'^doc_info', views.doc_info, name='doc_info'),       
         )
