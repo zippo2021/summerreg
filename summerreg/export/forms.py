da0693342be69391713a6b9e506f9e8316bbfc03
alex <uraharaLL@yandex.ru>
2014-04-07 17:25:18 +0400

    m

--- summerreg/export/forms.py
+++ summerreg/export/forms.py
@@ -5,8 +5,7 @@ from dashboard.models import UserData
 
 class showdb_form(forms.Form):
     #dynamic form, fields: checkboxes 
-    CITY_CHOICES = list(UserData.objects.values_list('city', flat = True))
-    CITY_CHOICES.append('Все')
+    CITY_CHOICES = list(UserData.objects.values_list('city', flat = True))+['All']
     CITY_CHOICES = [(each, each) for each in CITY_CHOICES]
     cities = forms.MultipleChoiceField(required=True, widget=forms.CheckboxSelectMultiple, choices=CITY_CHOICES, label='Cities(to be trans)')
 
